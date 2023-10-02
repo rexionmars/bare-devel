d6eb7ba91c6ee2e89e24035f092b838a93f2975e
João Leonardi <opensource.leonardi@gmail.com>
2023-10-01 21:57:36 -0300

    del: removed unnecessary files

--- crystal-embbed/probe/std/src/lib_c/x86_64-windows-msvc/c/heapapi.cr
+++
@@ -1,8 +0,0 @@
-require "c/winnt"
-
-lib LibC
-  fun GetProcessHeap : HANDLE
-  fun HeapAlloc(hHeap : HANDLE, dwFlags : DWORD, dwBytes : SizeT) : Void*
-  fun HeapReAlloc(hHeap : HANDLE, dwFlags : DWORD, lpMem : Void*, dwBytes : SizeT) : Void*
-  fun HeapFree(hHeap : HANDLE, dwFlags : DWORD, lpMem : Void*) : BOOL
-end
