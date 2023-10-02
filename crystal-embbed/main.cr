313c15cdb163e73367b7d4407c9bb20b0bb81839
João Leonardi <opensource.leonardi@gmail.com>
2023-10-01 21:41:30 -0300

    feat: add crystal core LLVM

--- crystal-embbed/main.cr
+++
@@ -1,3 +0,0 @@
-#hello word
-
-puts "Hello Word"
--- crystal-embbed/probe/src/main.cr
+++ crystal-embbed/probe/src/main.cr
@@ -1,3 +1,12 @@
-require "levenshtein"
+#!/usr/bin/env crystal
 
-puts "Hello, word!"
+require "http/server"
+
+server = HTTP::Server.new do |context|
+  context.response.content_type = "text/plain"
+  context.response.print "Hello, World! The time is #{Time.local}"
+end
+
+address = server.bind_tcp 6969
+puts "Listening on http://#{address}"
+server.listen
